7822bbce7d24c62b8f68c9351a7aa73284111c39
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-07-28 11:51:27 +0530

    Update content.js

--- content.js
+++ content.js
@@ -30,38 +30,3 @@ function renderProducts(products) {
     }
   });
 }
-
-function dynamicSection(ob) {
-  let boxDiv = document.createElement("div");
-  boxDiv.id = "box";
-
-  let boxLink = document.createElement("a");
-  boxLink.href = "/contentDetails.html?" + ob.id;
-
-  let imgTag = document.createElement("img");
-  imgTag.src = ob.preview;
-
-  let detailsDiv = document.createElement("div");
-  detailsDiv.id = "details";
-
-  let h3 = document.createElement("h3");
-  let h3Text = document.createTextNode(ob.name);
-  h3.appendChild(h3Text);
-
-  let h4 = document.createElement("h4");
-  let h4Text = document.createTextNode(ob.brand);
-  h4.appendChild(h4Text);
-
-  let h2 = document.createElement("h2");
-  let h2Text = document.createTextNode("rs  " + ob.price);
-  h2.appendChild(h2Text);
-
-  boxDiv.appendChild(boxLink);
-  boxLink.appendChild(imgTag);
-  boxLink.appendChild(detailsDiv);
-  detailsDiv.appendChild(h3);
-  detailsDiv.appendChild(h4);
-  detailsDiv.appendChild(h2);
-
-  return boxDiv;
-}
