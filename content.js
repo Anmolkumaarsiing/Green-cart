656c49d1dbb450f714d3d8cd9a78bff0f22d7449
Saurabh5233 <124551411+Saurabh5233@users.noreply.github.com>
2024-10-05 15:47:56 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,19 +1,3 @@
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
-
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-
-
-
-
 // Function to create a dynamic section for grocery or scrap items
 function dynamicSection(ob) {
   let boxDiv = document.createElement("div");
