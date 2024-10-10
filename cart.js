e7e2a68ae4f135d418ab524950728e678491a727
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-10-10 19:35:59 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,139 +1,155 @@
 console.clear();
 
-if(document.cookie.indexOf(',counter=')>=0)
-{
-    let counter = document.cookie.split(',')[1].split('=')[1]
-    document.getElementById("badge").innerHTML = counter
+if (document.cookie.indexOf(',counter=') >= 0) {
+    let counter = document.cookie.split(',')[1].split('=')[1];
+    document.getElementById("badge").innerHTML = counter;
 }
 
+let cartContainer = document.getElementById('cartContainer');
 
-let cartContainer = document.getElementById('cartContainer')
-
-let boxContainerDiv = document.createElement('div')
-boxContainerDiv.id = 'boxContainer'
+let boxContainerDiv = document.createElement('div');
+boxContainerDiv.id = 'boxContainer';
 
 // DYNAMIC CODE TO SHOW THE SELECTED ITEMS IN YOUR CART
-function dynamicCartSection(ob,itemCounter)
-{
-    let boxDiv = document.createElement('div')
-    boxDiv.id = 'box'
-    boxContainerDiv.appendChild(boxDiv)
+function dynamicCartSection(ob, itemCounter) {
+    let boxDiv = document.createElement('div');
+    boxDiv.id = 'box';
+    boxContainerDiv.appendChild(boxDiv);
 
-    let boxImg = document.createElement('img')
-    boxImg.src = ob.preview
-    boxDiv.appendChild(boxImg)
+    let boxImg = document.createElement('img');
+    boxImg.src = ob.preview;
+    boxDiv.appendChild(boxImg);
 
-    let boxh3 = document.createElement('h3')
-    let h3Text = document.createTextNode(ob.name + ' × ' + itemCounter)
-    // let h3Text = document.createTextNode(ob.name)
-    boxh3.appendChild(h3Text)
-    boxDiv.appendChild(boxh3)
+    let boxh3 = document.createElement('h3');
+    let h3Text = document.createTextNode(ob.name + ' × ' + itemCounter);
+    boxh3.appendChild(h3Text);
+    boxDiv.appendChild(boxh3);
 
-    let boxh4 = document.createElement('h4')
-    let h4Text = document.createTextNode('Amount: Rs' + ob.price)
-    boxh4.appendChild(h4Text)
-    boxDiv.appendChild(boxh4)
+    let boxh4 = document.createElement('h4');
+    let h4Text = document.createTextNode('Amount: Rs ' + (ob.price * itemCounter));
+    boxh4.appendChild(h4Text);
+    boxDiv.appendChild(boxh4);
 
-    // console.log(boxContainerDiv);
-
-    buttonLink.appendChild(buttonText)
-    cartContainer.appendChild(boxContainerDiv)
-    cartContainer.appendChild(totalContainerDiv)
-    // let cartMain = document.createElement('div')
-    // cartmain.id = 'cartMainContainer'
-    // cartMain.appendChild(totalContainerDiv)
-
-    return cartContainer
+    cartContainer.appendChild(boxContainerDiv);
 }
 
-let totalContainerDiv = document.createElement('div')
-totalContainerDiv.id = 'totalContainer'
+let totalContainerDiv = document.createElement('div');
+totalContainerDiv.id = 'totalContainer';
 
-let totalDiv = document.createElement('div')
-totalDiv.id = 'total'
-totalContainerDiv.appendChild(totalDiv)
+let totalDiv = document.createElement('div');
+totalDiv.id = 'total';
+totalContainerDiv.appendChild(totalDiv);
 
-let totalh2 = document.createElement('h2')
-let h2Text = document.createTextNode('Total Amount')
-totalh2.appendChild(h2Text)
-totalDiv.appendChild(totalh2)
+let totalh2 = document.createElement('h2');
+let h2Text = document.createTextNode('Total Amount');
+totalh2.appendChild(h2Text);
+totalDiv.appendChild(totalh2);
 
 // TO UPDATE THE TOTAL AMOUNT
-function amountUpdate(amount)
-{
-    let totalh4 = document.createElement('h4')
-    // let totalh4Text = document.createTextNode(amount)
-    let totalh4Text = document.createTextNode('Amount: Rs ' + amount)
-    totalh4Text.id = 'toth4'
-    totalh4.appendChild(totalh4Text)
-    totalDiv.appendChild(totalh4)
-    totalDiv.appendChild(buttonDiv)
-    console.log(totalh4);
+function amountUpdate(amount) {
+    let totalh4 = document.createElement('h4');
+    let totalh4Text = document.createTextNode('Amount: Rs ' + amount);
+    totalh4.appendChild(totalh4Text);
+    totalDiv.appendChild(totalh4);
 }
 
+// Place Order Button
+let buttonDiv = document.createElement('div');
+buttonDiv.id = 'button';
+totalDiv.appendChild(buttonDiv);
 
-let buttonDiv = document.createElement('div')
-buttonDiv.id = 'button'
-totalDiv.appendChild(buttonDiv)
+let buttonTag = document.createElement('button');
+buttonDiv.appendChild(buttonTag);
 
-let buttonTag = document.createElement('button')
-buttonDiv.appendChild(buttonTag)
+let buttonText = document.createTextNode('Place Order');
+buttonTag.appendChild(buttonText);
 
-let buttonLink = document.createElement('a')
-buttonLink.href = '/orderPlaced.html?'
-buttonTag.appendChild(buttonLink)
-
-buttonText = document.createTextNode('Place Order')
-buttonTag.onclick = function()
-{
-    console.log("clicked")
-}  
-//dynamicCartSection()
-// console.log(dynamicCartSection());
+buttonTag.onclick = function () {
+    // Call Razorpay payment integration here
+    createOrder(); // Function to create Razorpay order
+};
 
 // BACKEND CALL
-let httpRequest = new XMLHttpRequest()
-let totalAmount = 0
-httpRequest.onreadystatechange = function()
-{
-    if(this.readyState === 4)
-    {
-        if(this.status == 200)
-        {
-            // console.log('call successful');
-            contentTitle = JSON.parse(this.responseText)
+let httpRequest = new XMLHttpRequest();
+let totalAmount = 0;
+httpRequest.onreadystatechange = function () {
+    if (this.readyState === 4) {
+        if (this.status == 200) {
+            let contentTitle = JSON.parse(this.responseText);
+            let counter = Number(document.cookie.split(',')[1].split('=')[1]);
+            document.getElementById("totalItem").innerHTML = ('Total Items: ' + counter);
 
-            let counter = Number(document.cookie.split(',')[1].split('=')[1])
-            document.getElementById("totalItem").innerHTML = ('Total Items: ' + counter)
-
-            let item = document.cookie.split(',')[0].split('=')[1].split(" ")
-            console.log(counter)
-            console.log(item)
-
-            let i;
-            let totalAmount = 0
-            for(i=0; i<counter; i++)
-            {
-                let itemCounter = 1
-                for(let j = i+1; j<counter; j++)
-                {   
-                    if(Number(item[j]) == Number(item[i]))
-                    {
-                        itemCounter +=1;
+            let item = document.cookie.split(',')[0].split('=')[1].split(" ");
+            let totalAmount = 0;
+            for (let i = 0; i < counter; i++) {
+                let itemCounter = 1;
+                for (let j = i + 1; j < counter; j++) {
+                    if (Number(item[j]) == Number(item[i])) {
+                        itemCounter += 1;
                     }
                 }
-                totalAmount += Number(contentTitle[item[i]-1].price) * itemCounter
-                dynamicCartSection(contentTitle[item[i]-1],itemCounter)
-                i += (itemCounter-1)
+                totalAmount += Number(contentTitle[item[i] - 1].price) * itemCounter;
+                dynamicCartSection(contentTitle[item[i] - 1], itemCounter);
+                i += (itemCounter - 1);
             }
-            amountUpdate(totalAmount)
+            amountUpdate(totalAmount);
         }
+    } else {
+        console.log('call failed!');
     }
-        else
-        {
-            console.log('call failed!');
-        }
 }
 
-httpRequest.open('GET', 'https://669e2f559a1bda368005b99b.mockapi.io/Product/ProducData', true)
-httpRequest.send()
+httpRequest.open('GET', 'https://669e2f559a1bda368005b99b.mockapi.io/Product/ProducData', true);
+httpRequest.send();
+
+// Function to create Razorpay order
+function createOrder() {
+    let amount = totalAmount * 100; // Amount should be in paise
+    let orderData = {
+        amount: amount,
+        currency: "INR",
+        receipt: "receipt#1"
+    };
+
+    // Create order on the server
+    fetch("/create-order", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(orderData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Open Razorpay payment modal
+        const options = {
+            key: "rzp_test_4sMuXigiNls8Jr", // Your Razorpay key ID
+            amount: data.amount, // Amount in paise
+            currency: data.currency,
+            name: "Your Company Name",
+            description: "Order Description",
+            image: "https://yourlogo.com/logo.png",
+            order_id: data.id, // Order ID from Razorpay
+            handler: function (response) {
+                alert("Payment successful!");
+                console.log(response);
+                // Redirect to order placed page
+                window.location.href = "orderplaced.html"; // Redirect to your order placed page
+            },
+            prefill: {
+                name: "Customer Name",
+                email: "customer@example.com",
+                contact: "9999999999"
+            },
+            theme: {
+                color: "#F37254"
+            }
+        };
+
+        const rzp1 = new Razorpay(options);
+        rzp1.open();
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+}
