2474e0ca34cc631b59536c21327811d611bec052
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-10-10 19:06:44 +0530

    Update contentDetails.js

--- contentDetails.js
+++ contentDetails.js
@@ -1,81 +1,88 @@
+console.clear()
 
-console.clear();
+let id = location.search.split('?')[1]
+console.log(id)
 
-let id = location.search.split('?')[1];
-console.log(id);
-
-if (document.cookie.indexOf(',counter=') >= 0) {
-    let counter = document.cookie.split(',')[1].split('=')[1];
-    document.getElementById("badge").innerHTML = counter;
+if(document.cookie.indexOf(',counter=')>=0)
+{
+    let counter = document.cookie.split(',')[1].split('=')[1]
+    document.getElementById("badge").innerHTML = counter
 }
 
-function dynamicContentDetails(ob) {
-    let mainContainer = document.createElement('div');
-    mainContainer.id = 'containerD';
+function dynamicContentDetails(ob)
+{
+    let mainContainer = document.createElement('div')
+    mainContainer.id = 'containerD'
     document.getElementById('containerProduct').appendChild(mainContainer);
 
-    let imageSectionDiv = document.createElement('div');
-    imageSectionDiv.id = 'imageSection';
+    let imageSectionDiv = document.createElement('div')
+    imageSectionDiv.id = 'imageSection'
 
-    let imgTag = document.createElement('img');
-    imgTag.id = 'imgDetails';
-    imgTag.src = ob.preview;
+    let imgTag = document.createElement('img')
+     imgTag.id = 'imgDetails'
+     //imgTag.id = ob.photos
+     imgTag.src = ob.preview
 
-    imageSectionDiv.appendChild(imgTag);
+    imageSectionDiv.appendChild(imgTag)
 
-    let productDetailsDiv = document.createElement('div');
-    productDetailsDiv.id = 'productDetails';
+    let productDetailsDiv = document.createElement('div')
+    productDetailsDiv.id = 'productDetails'
 
-    let h1 = document.createElement('h1');
-    let h1Text = document.createTextNode(ob.name);
-    h1.appendChild(h1Text);
+    // console.log(productDetailsDiv);
 
-    let h4 = document.createElement('h4');
-    let h4Text = document.createTextNode(ob.brand);
-    h4.appendChild(h4Text);
+    let h1 = document.createElement('h1')
+    let h1Text = document.createTextNode(ob.name)
+    h1.appendChild(h1Text)
 
-    let detailsDiv = document.createElement('div');
-    detailsDiv.id = 'details';
+    let h4 = document.createElement('h4')
+    let h4Text = document.createTextNode(ob.brand)
+    h4.appendChild(h4Text)
+    console.log(h4);
 
-    let h3DetailsDiv = document.createElement('h3');
-    let h3DetailsText = document.createTextNode('Rs ' + ob.price);
-    h3DetailsDiv.appendChild(h3DetailsText);
+    let detailsDiv = document.createElement('div')
+    detailsDiv.id = 'details'
 
-    let h3 = document.createElement('h3');
-    let h3Text = document.createTextNode('Description');
-    h3.appendChild(h3Text);
+    let h3DetailsDiv = document.createElement('h3')
+    let h3DetailsText = document.createTextNode('Rs ' + ob.price)
+    h3DetailsDiv.appendChild(h3DetailsText)
 
-    let para = document.createElement('p');
-    let paraText = document.createTextNode(ob.description);
-    para.appendChild(paraText);
+    let h3 = document.createElement('h3')
+    let h3Text = document.createTextNode('Description')
+    h3.appendChild(h3Text)
 
-    let productPreviewDiv = document.createElement('div');
-    productPreviewDiv.id = 'productPreview';
+    let para = document.createElement('p')
+    let paraText = document.createTextNode(ob.description)
+    para.appendChild(paraText)
 
-    let h3ProductPreviewDiv = document.createElement('h3');
-    let h3ProductPreviewText = document.createTextNode('Product Preview');
-    h3ProductPreviewDiv.appendChild(h3ProductPreviewText);
-    productPreviewDiv.appendChild(h3ProductPreviewDiv);
+    let productPreviewDiv = document.createElement('div')
+    productPreviewDiv.id = 'productPreview'
 
-    for (let i = 0; i < ob.photos.length; i++) {
-        let imgTagProductPreviewDiv = document.createElement('img');
-        imgTagProductPreviewDiv.id = 'previewImg';
-        imgTagProductPreviewDiv.src = ob.photos[i];
-        imgTagProductPreviewDiv.onclick = function () {
-            console.log("clicked " + this.src);
-            imgTag.src = this.src;
-            document.getElementById("imgDetails").src = this.src;
-        };
-        productPreviewDiv.appendChild(imgTagProductPreviewDiv);
+    let h3ProductPreviewDiv = document.createElement('h3')
+    let h3ProductPreviewText = document.createTextNode('Product Preview')
+    h3ProductPreviewDiv.appendChild(h3ProductPreviewText)
+    productPreviewDiv.appendChild(h3ProductPreviewDiv)
+
+    let i;
+    for(i=0; i<ob.photos.length; i++)
+    {
+        let imgTagProductPreviewDiv = document.createElement('img')
+        imgTagProductPreviewDiv.id = 'previewImg'
+        imgTagProductPreviewDiv.src = ob.photos[i]
+        imgTagProductPreviewDiv.onclick = function(event)
+        {
+            console.log("clicked" + this.src)
+            imgTag.src = ob.photos[i]
+            document.getElementById("imgDetails").src = this.src 
+            
+        }
+        productPreviewDiv.appendChild(imgTagProductPreviewDiv)
     }
 
-    
     let buttonDiv = document.createElement('div')
     buttonDiv.id = 'button'
 
     let buttonTag = document.createElement('button')
     buttonDiv.appendChild(buttonTag)
-
 
     buttonText = document.createTextNode('Add to Cart')
     buttonTag.onclick  =   function()
@@ -136,5 +143,5 @@ let httpRequest = new XMLHttpRequest()
     }
 }
 
-httpRequest.open('GET', 'https://669e2f559a1bda368005b99b.mockapi.io/Product/ProducData/' + id, true);
-httpRequest.send();
+httpRequest.open('GET', 'https://669e2f559a1bda368005b99b.mockapi.io/Product/ProducData'+id, true)
+httpRequest.send()  
