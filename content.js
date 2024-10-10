1297733494ca9de4cc401502434fc70b86c50d25
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-10-10 19:12:10 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,38 +1,37 @@
-
 // Function to create a dynamic section for grocery or scrap items
 function dynamicSection(ob) {
-let boxDiv = document.createElement("div");
-boxDiv.id = "box";
+  let boxDiv = document.createElement("div");
+  boxDiv.id = "box";
 
-let boxLink = document.createElement("a");
-boxLink.href = "/contentDetails.html?" + ob.id;
+  let boxLink = document.createElement("a");
+  boxLink.href = "/contentDetails.html?" + ob.id;
 
-let imgTag = document.createElement("img");
-imgTag.src = ob.preview;
+  let imgTag = document.createElement("img");
+  imgTag.src = ob.preview;
 
-let detailsDiv = document.createElement("div");
-detailsDiv.id = "details";
+  let detailsDiv = document.createElement("div");
+  detailsDiv.id = "details";
 
-let h3 = document.createElement("h3");
-let h3Text = document.createTextNode(ob.name);
-h3.appendChild(h3Text);
+  let h3 = document.createElement("h3");
+  let h3Text = document.createTextNode(ob.name);
+  h3.appendChild(h3Text);
 
-let h4 = document.createElement("h4");
-let h4Text = document.createTextNode(ob.brand);
-h4.appendChild(h4Text);
+  let h4 = document.createElement("h4");
+  let h4Text = document.createTextNode(ob.brand);
+  h4.appendChild(h4Text);
 
-let h2 = document.createElement("h2");
-let h2Text = document.createTextNode("rs  " + ob.price);
-h2.appendChild(h2Text);
+  let h2 = document.createElement("h2");
+  let h2Text = document.createTextNode("rs  " + ob.price);
+  h2.appendChild(h2Text);
 
-boxDiv.appendChild(boxLink);
-boxLink.appendChild(imgTag);
-boxLink.appendChild(detailsDiv);
-detailsDiv.appendChild(h3);
-detailsDiv.appendChild(h4);
-detailsDiv.appendChild(h2);
+  boxDiv.appendChild(boxLink);
+  boxLink.appendChild(imgTag);
+  boxLink.appendChild(detailsDiv);
+  detailsDiv.appendChild(h3);
+  detailsDiv.appendChild(h4);
+  detailsDiv.appendChild(h2);
 
-return boxDiv;
+  return boxDiv;
 }
 
 let containerGrocery = document.getElementById("containerGrocery");
@@ -42,29 +41,29 @@ let containerScrap = document.getElementById("containerScrap");
 let httpRequest = new XMLHttpRequest();
 
 httpRequest.onreadystatechange = function() {
-if (this.readyState === 4) {
-if (this.status === 200) {
-let contentTitle = JSON.parse(this.responseText);
-if (document.cookie.indexOf(",counter=") >= 0) {
-let counter = document.cookie.split(",")[1].split("=")[1];
-document.getElementById("badge").innerHTML = counter;
-}
-for (let i = 0; i < contentTitle.length; i++) {
-if (contentTitle[i].isScrap) {
-containerScrap.appendChild(dynamicSection(contentTitle[i]));
-} else {
-containerGrocery.appendChild(dynamicSection(contentTitle[i]));
-}
-}
-} else {
-console.log("Call failed!");
-}
-}
+  if (this.readyState === 4) {
+    if (this.status === 200) {
+      let contentTitle = JSON.parse(this.responseText);
+      if (document.cookie.indexOf(",counter=") >= 0) {
+        let counter = document.cookie.split(",")[1].split("=")[1];
+        document.getElementById("badge").innerHTML = counter;
+      }
+      for (let i = 0; i < contentTitle.length; i++) {
+        if (contentTitle[i].isScrap) {
+          containerScrap.appendChild(dynamicSection(contentTitle[i]));
+        } else {
+          containerGrocery.appendChild(dynamicSection(contentTitle[i]));
+        }
+      }
+    } else {
+      console.log("Call failed!");
+    }
+  }
 };
 
 httpRequest.open(
-"GET",
-"https://669e2f559a1bda368005b99b.mockapi.io/Product/ProducData",
-true
+  "GET",
+  "https://669e2f559a1bda368005b99b.mockapi.io/Product/ProducData",
+  true
 );
 httpRequest.send();
