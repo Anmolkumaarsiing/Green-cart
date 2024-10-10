3278e068f838c5b57a6dfede0288426d8066d2d1
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-10-11 01:16:02 +0530

    Update contentDetails.js

--- contentDetails.js
+++ contentDetails.js
@@ -68,25 +68,32 @@ function dynamicContentDetails(ob) {
         productPreviewDiv.appendChild(imgTagProductPreviewDiv);
     }
 
-    let buttonDiv = document.createElement('div');
+     let buttonDiv = document.createElement('div');
     buttonDiv.id = 'button';
 
-    let buttonTag = document.createElement('button');
-    buttonDiv.appendChild(buttonTag);
-
-    let buttonText = document.createTextNode('Add to Cart');
-    buttonTag.onclick = function () {
-        let order = id + " ";
-        let counter = 1;
-        if (document.cookie.indexOf(',counter=') >= 0) {
-            order = id + " " + document.cookie.split(',')[0].split('=')[1];
-            counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1;
-        }
-        document.cookie = "orderId=" + order + ",counter=" + counter;
-        document.getElementById("badge").innerHTML = counter;
-        console.log(document.cookie);
-    };
-    buttonTag.appendChild(buttonText);
+    // Check if the item is a scrap item or a grocery item
+    if (ob.isScrap) {
+        let requestPickupButton = document.createElement('button');
+        requestPickupButton.textContent = 'Request Pickup';
+        requestPickupButton.onclick = function() {
+            window.location.href = '/pickupRequestForm.html?id=' + id; // Redirect to pickup request form
+        };
+        buttonDiv.appendChild(requestPickupButton);
+    } else {
+        let buttonTag = document.createElement('button');
+        buttonTag.textContent = 'Add to Cart';
+        buttonTag.onclick = function () {
+            let order = id + " ";
+            let counter = 1;
+            if (document.cookie.indexOf(',counter=') >= 0) {
+                order = id + " " + document.cookie.split(',')[0].split('=')[1];
+                counter = Number(document.cookie.split(',')[1].split('=')[1]) + 1;
+            }
+            document.cookie = "orderId=" + order + ",counter=" + counter;
+            document.getElementById("badge").innerHTML = counter;
+        };
+        buttonDiv.appendChild(buttonTag);
+    }
 
     // Append all sections to the main container
     mainContainer.appendChild(imageSectionDiv);
