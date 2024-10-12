2597508ef73434b515814271002004a57510405c
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-10-12 15:47:43 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,29 @@
+// auth.js
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCrSBQoJDG9Cn5t2vsWNvDDkDQJm1UxTgk",
+    authDomain: "green--cart.firebaseapp.com",
+    databaseURL: "https://green--cart-default-rtdb.firebaseio.com",
+    projectId: "green--cart",
+    storageBucket: "green--cart.appspot.com",
+    messagingSenderId: "997863065",
+    appId: "1:997863065:web:1716dad07cdbe649e81208",
+    measurementId: "G-56BY927ZLY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Check if the user is logged in
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        // If the user is not signed in, redirect to the login page
+        window.location.href = '/login/login.html';
+    }
+});
