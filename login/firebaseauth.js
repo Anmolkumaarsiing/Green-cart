40b5192a2b66ba776b6dc9dc31dbaff4f3e15aea
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-10-18 17:29:39 +0530

    Update firebaseauth.js

--- login/firebaseauth.js
+++ login/firebaseauth.js
@@ -1,9 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
-// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCrSBQoJDG9Cn5t2vsWNvDDkDQJm1UxTgk",
     authDomain: "green--cart.firebaseapp.com",
@@ -13,89 +12,104 @@ const firebaseConfig = {
     messagingSenderId: "997863065",
     appId: "1:997863065:web:1716dad07cdbe649e81208",
     measurementId: "G-56BY927ZLY"
-  };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore();
 
-// Check if the user is already logged in
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // If user is logged in, redirect to the homepage
-    window.location.href = '../homepage.html';
-  }
-});
-
-// Function to display messages
+// Function to show messages
 function showMessage(message, divId) {
-  var messageDiv = document.getElementById(divId);
-  messageDiv.style.display = "block";
-  messageDiv.innerHTML = message;
-  messageDiv.style.opacity = 1;
-  setTimeout(function () {
-    messageDiv.style.opacity = 0;
-  }, 5000);
+    var messageDiv = document.getElementById(divId);
+    messageDiv.style.display = "block";
+    messageDiv.innerHTML = message;
+    messageDiv.style.opacity = 1;
+    setTimeout(function () {
+        messageDiv.style.opacity = 0;
+    }, 5000);
 }
 
-// Sign-up functionality
+// Sign Up functionality
 const signUp = document.getElementById('submitSignUp');
 signUp.addEventListener('click', (event) => {
-  event.preventDefault();
-  const email = document.getElementById('rEmail').value;
-  const password = document.getElementById('rPassword').value;
-  const firstName = document.getElementById('fName').value;
-  const lastName = document.getElementById('lName').value;
+    event.preventDefault();
+    const email = document.getElementById('rEmail').value;
+    const password = document.getElementById('rPassword').value;
+    const firstName = document.getElementById('fName').value;
+    const lastName = document.getElementById('lName').value;
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      const userData = {
-        email: email,
-        firstName: firstName,
-        lastName: lastName
-      };
-      showMessage('Account Created Successfully', 'signUpMessage');
-      const docRef = doc(db, "users", user.uid);
-      setDoc(docRef, userData)
-        .then(() => {
-          window.location.href = 'login.html';
+    const auth = getAuth();
+    const db = getFirestore();
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            const userData = {
+                email: email,
+                firstName: firstName,
+                lastName: lastName
+            };
+            showMessage('Account Created Successfully', 'signUpMessage');
+            const docRef = doc(db, "users", user.uid);
+            setDoc(docRef, userData)
+                .then(() => {
+                    window.location.href = '/login/login.html';
+                })
+                .catch((error) => {
+                    console.error("Error writing document", error);
+                });
         })
         .catch((error) => {
-          console.error("Error writing document", error);
+            const errorCode = error.code;
+            if (errorCode == 'auth/email-already-in-use') {
+                showMessage('Email Address Already Exists !!!', 'signUpMessage');
+            } else {
+                showMessage('Unable to create User', 'signUpMessage');
+            }
         });
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      if (errorCode === 'auth/email-already-in-use') {
-        showMessage('Email Address Already Exists !!!', 'signUpMessage');
-      } else {
-        showMessage('Unable to create User', 'signUpMessage');
-      }
-    });
 });
 
-// Sign-in functionality
+// Sign In functionality
 const signIn = document.getElementById('submitSignIn');
 signIn.addEventListener('click', (event) => {
-  event.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const auth = getAuth();
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      showMessage('Login is successful', 'signInMessage');
-      const user = userCredential.user;
-      localStorage.setItem('loggedInUserId', user.uid);
-      window.location.href = '../homepage.html';
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      if (errorCode === 'auth/invalid-credential') {
-        showMessage('Incorrect Email or Password', 'signInMessage');
-      } else {
-        showMessage('Account does not Exist', 'signInMessage');
-      }
-    });
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            showMessage('Login is successful', 'signInMessage');
+            const user = userCredential.user;
+            localStorage.setItem('loggedInUserId', user.uid);
+            window.location.href = '../homepage.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            if (errorCode === 'auth/invalid-credential') {
+                showMessage('Incorrect Email or Password', 'signInMessage');
+            } else {
+                showMessage('Account does not Exist', 'signInMessage');
+            }
+        });
+});
+
+// Google Sign In functionality
+const googleSignInButton = document.getElementById('googleSignInButton');
+googleSignInButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            console.log("Google Sign-In successful:", user);
+            showMessage('Login is successful', 'signInMessage');
+            localStorage.setItem('loggedInUserId', user.uid);
+            window.location.href = '../homepage.html';
+        })
+        .catch((error) => {
+            console.error("Error during Google Sign-In:", error);
+            showMessage('Error during Google Sign-In', 'signInMessage');
+        });
 });
